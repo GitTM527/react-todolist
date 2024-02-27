d69ab84f80969fff41cdbf0e9119688ca7054381
GitTM527 <100084216+GitTM527@users.noreply.github.com>
2024-02-27 03:49:23 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://gittm527.github.io/todolist-reactjs/my-react/"
+  base: "https://gittm527.github.io/react-todolist/"
 })
